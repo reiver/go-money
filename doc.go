c7f3c91303dd04e4b8d2176b138c5420cb1d9053
Charles Iliya Krempeaux <iamreiver@gmail.com>
2015-09-11 13:54:01 -0700

    adding some docs

---
+++ doc.go
@@ -0,0 +1,11 @@
+/*
+Package money provides a way of dealing with money in type safe way, including parsing from strings.
+
+Parsing
+
+	canadianMonday, err := money.ParseCAD("$12.34")
+
+	usMonday, err := money.ParseUSD("$12.34")
+
+*/
+package httplog
