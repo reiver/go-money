ff476ee2d36706e2d0a8c842942a700fda4d17a6
Charles Iliya Krempeaux <iamreiver@gmail.com>
2015-10-20 11:27:39 -0700

    made it so money.CAD and money.USD fit the database/sql/driver.Valuer interface. this makes it so they can be used for INSERTs and UPDATEs on a database without having to call .String() on them.

---
+++ value.go
@@ -0,0 +1,22 @@
+package money
+
+
+import (
+	"database/sql/driver"
+)
+
+
+
+
+// Value is to make it so money.CAD fits the database/sql/driver.Valuer interface.
+func (m CAD) Value() (driver.Value, error) {
+	s := m.String()
+	return s, nil
+}
+
+
+// Value is to make it so money.USD fits the database/sql/driver.Valuer interface.
+func (m USD) Value() (driver.Value, error) {
+	s := m.String()
+	return s, nil
+}
